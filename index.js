0b548f30652435ea7dc40bd2064643afc27a7cd8
Alexandre Philibert <philibert.alexandre@gmail.com>
2020-05-29 22:32:01 +0200

    Made pages use template html file Changed file structure of articles Added inline svg

--- index.js
+++ index.js
@@ -1,23 +1,54 @@
-let fs = require('fs');
+const fs = require('fs').promises;
+const fsExtra = require('fs-extra');
+const MarkdownIt = require('markdown-it');
 
-let MarkdownIt = require('markdown-it');
+const READ_FILE_OPTIONS = {
+    encoding: 'UTF-8'
+};
 
-let markdown = new MarkdownIt();
+setup();
+buildArticles();
+buildPages();
+copyAssets();
 
-if(!fs.existsSync('./build/articles')) {
-    fs.mkdirSync('./build/articles');
+async function setup() {
+    await fs.access('./build/articles').catch(_ => {
+        fs.mkdir('./build/articles');
+    });
 }
 
-let files = fs.readdirSync('./src/articles');
+async function buildPageFromTemplate(filePath, destPath) {
+    let template = await fs.readFile('./src/template.html', READ_FILE_OPTIONS);
+    let file = await fs.readFile(filePath, READ_FILE_OPTIONS);
+    fs.writeFile(destPath, template.replace('{{main-content}}', file), READ_FILE_OPTIONS);
+}
 
-for (const file of files) {
-    let content = fs.readFileSync(`./src/articles/${file}`, { 
-        encoding: 'UTF-8' 
-    });
+async function buildArticles() {
+    let markdown = new MarkdownIt();
+    let folders = await fs.readdir('./src/articles');
+    let globalTemplate = (await fs.readFile('./src/template.html')).toString();
+    let articleTemplate = (await fs.readFile('./src/templates/article.html')).toString();
 
-    let rendered = markdown.render(content);
+    let template = globalTemplate.replace('{{main-content}}', articleTemplate)
 
-    fs.writeFileSync(`./build/articles/${file.replace('.md', '')}.html`, rendered, {
-        encoding: 'UTF-8'
-    });
+    for (const folder of folders) {
+        let content = await fs.readFile(`./src/articles/${folder}/${folder}.md`);
+        let renderedContent = markdown.render(content.toString());
+        let article = template.replace('{{content}}', renderedContent);
+
+        fs.writeFile(`./build/articles/${folder}.html`, article, READ_FILE_OPTIONS);
+    }
+}
+
+async function buildPages() {
+    let files = await fs.readdir('./src/pages');
+
+    for (const file of files) {
+        buildPageFromTemplate(`./src/pages/${file}`, `./build/${file}`);
+    }
+}
+
+async function copyAssets() {
+    fsExtra.copy('./src/styles.css', './build/styles.css', { recursive: true });
+    fsExtra.copy('./src/images', './build/images', { recursive: true });
 }
